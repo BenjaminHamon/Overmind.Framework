5460dbe265442d6b1f35ce6a55d08f823bd4e677
Benjamin Hamon <hamon.benjamin@gmail.com>
2015-07-05 12:13:01 +0200

    Adds Overmind.Core project using .NET 3.5

---
+++ Overmind.Core.Net35/Extensions/PathExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Overmind.Core.Extensions
+{
+	public static class PathExtensions
+	{
+		public static string Combine(params string[] pathCollection)
+		{
+			return pathCollection.Aggregate((firstPath, secondPath) => Path.Combine(firstPath, secondPath));
+		}
+	}
+}
