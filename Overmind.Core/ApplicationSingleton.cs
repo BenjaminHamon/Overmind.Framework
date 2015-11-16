1a3c1cc2e27c16c71f98055c004a5b53a2c3de94
Benjamin Hamon <hamon.benjamin@gmail.com>
2015-11-16 21:40:43 +0100

    Replaces Facade with ApplicationSingleton
    ApplicationSingleton is now used to access common dependencies such as
    logging, instead of the facade classes.

---
+++ Overmind.Core/ApplicationSingleton.cs
@@ -0,0 +1,25 @@
+﻿using Overmind.Core.Log;
+
+namespace Overmind.Core
+{
+	/// <summary>
+	/// <para>Application level object to access common dependencies such as logging.
+	/// These dependencies are required by pretty much every class
+	/// so this singleton provides a way to get the default instance for them.</para>
+	/// <para>You must initialize this object at the start of your program.</para>
+	/// </summary>
+	public static class ApplicationSingleton
+	{
+		private static ILogger logger;
+		public static ILogger Logger
+		{
+			get
+			{
+				if (logger == null)
+					throw new OvermindException("ApplicationSingleton.Logger is not initialized.");
+				return logger;
+			}
+			set { logger = value; }
+		}
+	}
+}
